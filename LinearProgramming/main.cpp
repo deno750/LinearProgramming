a5ed428034f623d5223c3d17b236fa729b0623e6
deno750 <deronjic.denis@gmail.com>
2019-11-16 20:32:12 +0100

    Initial Commit

---
+++ LinearProgramming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinearProgramming
+//
+//  Created by Denis Deronjic on 16/11/2019.
+//  Copyright © 2019 Denis Deronjic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
